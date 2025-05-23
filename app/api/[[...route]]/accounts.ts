3d87473b77898cbaffb983d34de520dadeb96cfc
Christian <Christian.m.panerio@gmail.com>
2025-05-23 15:59:50 +0800

    Completed the accounts edit and delete(Bulk-delete) function

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import { eq } from "drizzle-orm";
+import { and, eq, inArray } from "drizzle-orm";
 import { db } from "@/db/drizzle";
 import { createId } from "@paralleldrive/cuid2";
 import { accounts, insertAccountSchema } from "@/db/schema";
@@ -19,32 +19,70 @@ const app = new Hono()
         async (c) => {
             const auth = getAuth(c);
 
-            if(!auth?.userId){
-                return c.json({ error: "unauthorized"}, 401)
+            if (!auth?.userId) {
+                return c.json({ error: "unauthorized" }, 401)
             }
 
-        const data = await db
-            .select({
-                id: accounts.id,
-                name: accounts.name,
-            })
-            .from(accounts)
-            .where(eq(accounts.userId, auth.userId));
+            const data = await db
+                .select({
+                    id: accounts.id,
+                    name: accounts.name,
+                })
+                .from(accounts)
+                .where(eq(accounts.userId, auth.userId));
 
-        return c.json({ data });
-    })
+            return c.json({ data });
+        })
+    .get(
+        "/:id",
+        zValidator("param", z.object({
+            id: z.string().optional(),
+        })),
+        clerkMiddleware(),
+        async (c) => {
+            const auth = getAuth(c);
+            const { id } = c.req.valid("param");
+
+            if (!id) {
+                return c.json({ error: "Missing id" }, 400);
+            }
+
+            if (!auth?.userId) {
+                return c.json({ error: "Unauthorized" }, 401);
+            }
+
+            const [data] = await db
+                .select({
+                    id: accounts.id,
+                    name: accounts.name,
+                })
+                .from(accounts)
+                .where(
+                    and(
+                        eq(accounts.userId, auth.userId),
+                        eq(accounts.id, id)
+                    ),
+                );
+
+            if (!data) {
+                return c.json({ error: "Not found" }, 404);
+            }
+
+            return c.json({ data });
+        }
+    )
     .post(
         "/",
         clerkMiddleware(),
         zValidator(
-            "json", 
+            "json",
             createAccountSchema as unknown as z.ZodType
         ),
         async (c) => {
             const auth = getAuth(c);
             const values = c.req.valid("json");
 
-            if (!auth?.userId){
+            if (!auth?.userId) {
                 return c.json({ error: "unauthorized" }, 401);
             }
 
@@ -55,7 +93,123 @@ const app = new Hono()
             }).returning();
 
             return c.json({ data });
-        });
+        })
+    .post(
+        "/bulk-delete",
+        clerkMiddleware(),
+        zValidator(
+            "json",
+            z.object({
+                ids: z.array(z.string()),
+            }),
+        ),
+        async (c) => {
+            const auth = getAuth(c);
+            const values = c.req.valid("json");
+
+            if (!auth?.userId) {
+                return c.json({ error: "Unauthorized" }, 401)
+            }
+
+            const data = await db
+                .delete(accounts)
+                .where(
+                    and(
+                        eq(accounts.userId, auth.userId),
+                        inArray(accounts.id, values.ids)
+                    )
+                )
+                .returning({
+                    id: accounts.id,
+                });
+
+            return c.json({ data });
+        },
+    )
+    .patch(
+        "/:id",
+        clerkMiddleware(),
+        zValidator(
+            "param",
+            z.object({
+                id: z.string().optional(),
+            }),
+        ),
+        zValidator(
+            "json",
+            createAccountSchema as unknown as z.ZodType
+        ),
+        async (c) => {
+            const auth = getAuth(c);
+            const { id } = c.req.valid("param");
+            const values = c.req.valid("json");
+
+            if (!id) {
+                return c.json({ error: "Missing id" }, 400);
+            }
+
+            if (!auth?.userId) {
+                return c.json({ error: "Unauthorized" }, 401);
+            }
+
+            const [data] = await db
+                .update(accounts)
+                .set(values)
+                .where(
+                    and(
+                        eq(accounts.userId, auth.userId),
+                        eq(accounts.id, id)
+                    ),
+                )
+                .returning();
+
+            if (!data) {
+                return c.json({ error: "Not found" }, 404);
+            }
+
+            return c.json({ data });
+        },
+    )
+    .delete(
+        "/:id",
+        clerkMiddleware(),
+        zValidator(
+            "param",
+            z.object({
+                id: z.string().optional(),
+            }),
+        ),
+        async (c) => {
+            const auth = getAuth(c);
+            const { id } = c.req.valid("param");
+
+            if (!id) {
+                return c.json({ error: "Missing id" }, 400);
+            }
+
+            if (!auth?.userId) {
+                return c.json({ error: "Unauthorized" }, 401);
+            }
+
+            const [data] = await db
+                .delete(accounts)
+                .where(
+                    and(
+                        eq(accounts.userId, auth.userId),
+                        eq(accounts.id, id)
+                    ),
+                )
+                .returning({
+                    id: accounts.id,
+                });
+
+            if (!data) {
+                return c.json({ error: "Not found" }, 404);
+            }
+
+            return c.json({ data });
+        },
+    );
 
 
 export default app;
