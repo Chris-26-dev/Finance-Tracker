27b8a0390e8ad77ff196e93a7cf9ba16c368faba
Christian <Christian.m.panerio@gmail.com>
2025-05-28 17:42:24 +0800

    Implemented Summary API

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,15 +1,18 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 
+import summary from './summary';
 import accounts from "./accounts";
 import categories from './categories';
 import transactions from './transactions';
+
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api');
 
 const routes = app
+    .route("/summary", summary)
     .route("/accounts", accounts)
     .route("/categories", categories)
     .route("/transactions", transactions)
